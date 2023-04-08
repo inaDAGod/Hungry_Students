4dcfbcbf9747237fc48148edba3d961488dbcd29
Daniel171202 <daniel.aldazosa@gmail.com>
2023-04-07 22:43:09 -0400

    Base de Datos
    Se conectó a firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:rive_animation/screens/entryPoint/entry_point.dart';
 import 'package:rive_animation/screens/onboding/onboding_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+    );
+
   runApp(const MyApp());
 }
 
